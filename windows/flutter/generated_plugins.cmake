379b4ad72fb5388ea18c07df01b11d8ac3ca92f9
Jitesh <jiteshoffice1234-star@users.noreply.github.com>
2026-07-28 23:59:14 +0530

    Build release APK in CI and upload to GitHub Releases

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,11 +3,8 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  cloud_firestore
   connectivity_plus
   file_selector_windows
-  firebase_auth
-  firebase_core
   share_plus
   speech_to_text_windows
   url_launcher_windows
